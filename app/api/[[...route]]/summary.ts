13ff37945c46787129ef7a7f66ef0fa182798674
gmenez <73724162+germanjimenezz18@users.noreply.github.com>
2024-09-30 07:59:31 +0200

    Summary Chart 1

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -133,11 +133,11 @@ const app = new Hono().get(
       .select({
         date: transactions.date,
         income:
-          sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+          sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ABS(${transactions.amount}) ELSE 0 END)`.mapWith(
             Number
           ),
         expenses:
-          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+          sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ABS(${transactions.amount}) ELSE 0 END)`.mapWith(
             Number
           ),
       })
--- components/area-variant.tsx
+++ components/area-variant.tsx
@@ -1,11 +1,70 @@
+import { format } from "date-fns";
+
+import {
+  Tooltip,
+  XAxis,
+  AreaChart,
+  Area,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
+import CustomTooltip from "./custom-tooltip";
+
 type Props = {
-    data?: {
-      date: string;
-      income: number;
-      expenses: number;
-    }[];
-  };
-  
+  data?: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
 export default function AreaVariant({ data }: Props) {
-  return <div>area-variant</div>;
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <AreaChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <defs>
+          <linearGradient id="income" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="2%" stopColor="#2B6CB0" stopOpacity={0.8} />
+            <stop offset="98%" stopColor="#2B6CB0" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="expenses" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="2%" stopColor="#f43f5e" stopOpacity={0.8} />
+            <stop offset="98%" stopColor="#f43f5e" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(date) => format(date, "dd MMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={16}
+        />
+
+        <Tooltip content={<CustomTooltip/>}/>
+
+        <Area
+          type={"monotone"}
+          dataKey={"income"}
+          stackId={"income"}
+          strokeWidth={2}
+          stroke="#2B6CB0"
+          fill="url(#income)"
+          className="drop-shadow-sm"
+        />
+
+        <Area
+          type={"monotone"}
+          dataKey={"expenses"}
+          stackId={"expenses"}
+          strokeWidth={2}
+          stroke="#f43f5e"
+          fill="url(#expenses)"
+          className="drop-shadow-sm"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 }
