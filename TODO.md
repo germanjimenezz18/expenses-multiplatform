50c12260fe49dce559c6ad7ea573dd4d97283b9b
G. Jimenez <germanjimenezz18@gmail.com>
2026-02-15 04:10:26 +0100

    refactor: streamline loading components in AnalyticsPage and update TODO list

--- TODO.md
+++ TODO.md
@@ -1,11 +1,10 @@
 # APP TODO LIST
 
+- [ ] Enhance csv upload with AI review and categorization
 - [x] Add latest shadcn components and configuration
-- [ ] Upgrade to tailwind v4 (currently on v3 for shadcn compatibility)
+    - [ ] Upgrade to tailwind v4 (currently on v3 for shadcn compatibility)
 
-
-
-- [x] Agregar AI vercel SDK
+- [x] Add vercel AI SDK 
 - [ ] Sheet > input > actualizar placeholder cuando cambias el name y vuelves a abrir el sheet
 - [x] Importacion de gastos por CSV
-- [ ] Arreglar *amount* al actualizar una transaccion
+- [x] Fix update transaction amount
--- app/dashboard/analytics/page.tsx
+++ app/dashboard/analytics/page.tsx
@@ -8,31 +8,37 @@ import { SpendingPieLoading } from "@/components/spending-pie";
 export default function AnaliticsPage() {
   return (
     <div className="gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      <Suspense
-        fallback={
-          <div className="mb-8 grid grid-cols-1 gap-8 pb-2 lg:grid-cols-3">
-            <DataCardLoading />
-            <DataCardLoading />
-            <DataCardLoading />
-          </div>
-        }
-      >
+      <Suspense fallback={<DataGridLoading />}>
         <DataGrid />
       </Suspense>
-      <Suspense
-        fallback={
-          <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
-            <div className="col-span-1 lg:col-span-3 xl:col-span-4">
-              <ChartLoading />
-            </div>
-            <div className="col-span-1 lg:col-span-3 xl:col-span-2">
-              <SpendingPieLoading />
-            </div>
-          </div>
-        }
-      >
+      <Suspense fallback={<DashboardLoading />}>
         <DataCharts />
       </Suspense>
+    </div>
+  );
+}
+
+/* Skeleton */
+function DataGridLoading() {
+  return (
+    <div className="mb-8 grid grid-cols-1 gap-8 pb-2 lg:grid-cols-3">
+      <DataCardLoading />
+      <DataCardLoading />
+      <DataCardLoading />
+    </div>
+  );
+}
+
+/* Skeleton */
+function DashboardLoading() {
+  return (
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-6">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        <ChartLoading />
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        <SpendingPieLoading />
+      </div>
     </div>
   );
 }
