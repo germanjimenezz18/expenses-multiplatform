571e7f6f365d61219bc80f60a8339ab6074d5cba
gmenez <73724162+germanjimenezz18@users.noreply.github.com>
2026-02-26 21:45:07 +0100

    refactor: update index exports for accounts, categories, and transactions features to improve modularization and organization

--- features/account-balances/index.ts
+++ features/account-balances/index.ts
@@ -1,20 +1,20 @@
 // API hooks
-export { useGetAccountBalances } from "./api/use-get-account-balances";
-export { useGetAccountBalance } from "./api/use-get-account-balance";
-export { useCreateAccountBalance } from "./api/use-create-account-balance";
-export { useEditAccountBalance } from "./api/use-edit-account-balance";
-export { useDeleteAccountBalance } from "./api/use-delete-account-balance";
-export { useGetLatestBalance } from "./api/use-get-latest-balance";
-export { useGetExpectedBalance } from "./api/use-get-expected-balance";
-export { useBulkCreateBalances } from "./api/use-bulk-create-balances";
 
+export { useBulkCreateBalances } from "./api/use-bulk-create-balances";
+export { useCreateAccountBalance } from "./api/use-create-account-balance";
+export { useDeleteAccountBalance } from "./api/use-delete-account-balance";
+export { useEditAccountBalance } from "./api/use-edit-account-balance";
+export { useGetAccountBalance } from "./api/use-get-account-balance";
+export { useGetAccountBalances } from "./api/use-get-account-balances";
+export { useGetExpectedBalance } from "./api/use-get-expected-balance";
+export { useGetLatestBalance } from "./api/use-get-latest-balance";
+export { default as AccountBalanceForm } from "./components/account-balance-form";
+export { default as BalanceTrackerSheet } from "./components/balance-tracker-sheet";
+export { default as EditAccountBalanceSheet } from "./components/edit-account-balance-sheet";
+
+// Components
+export { default as NewAccountBalanceSheet } from "./components/new-account-balance-sheet";
 // Hooks
 export { useNewAccountBalance } from "./hooks/use-new-account-balance";
 export { useOpenAccountBalance } from "./hooks/use-open-account-balance";
 export { useOpenBalanceTracker } from "./hooks/use-open-balance-tracker";
-
-// Components
-export { default as NewAccountBalanceSheet } from "./components/new-account-balance-sheet";
-export { default as EditAccountBalanceSheet } from "./components/edit-account-balance-sheet";
-export { default as BalanceTrackerSheet } from "./components/balance-tracker-sheet";
-export { default as AccountBalanceForm } from "./components/account-balance-form";
--- features/categories/index.ts
+++ features/categories/index.ts
@@ -1,17 +1,16 @@
 // API hooks
+
+export { useBulkDeleteCategories } from "./api/use-bulk-delete-categories";
+export { useCreateCategory } from "./api/use-create-category";
+export { useDeleteCategory } from "./api/use-delete-category";
+export { useEditCategory } from "./api/use-edit-category";
 export { useGetCategories } from "./api/use-get-categories";
 export { useGetCategory } from "./api/use-get-category";
-export { useCreateCategory } from "./api/use-create-category";
-export { useEditCategory } from "./api/use-edit-category";
-export { useDeleteCategory } from "./api/use-delete-category";
-export { useBulkDeleteCategories } from "./api/use-bulk-delete-categories";
-
+export { default as ButtonSheetNewCategory } from "./components/button-sheet-new-category";
+export { default as CategoryForm } from "./components/category-form";
+export { default as EditCategorySheet } from "./components/edit-category-sheet";
+// Components
+export { default as NewCategorySheet } from "./components/new-category-sheet";
 // Hooks
 export { useNewCategory } from "./hooks/use-new-category";
 export { useOpenCategory } from "./hooks/use-open-category";
-
-// Components
-export { default as NewCategorySheet } from "./components/new-category-sheet";
-export { default as EditCategorySheet } from "./components/edit-category-sheet";
-export { default as ButtonSheetNewCategory } from "./components/button-sheet-new-category";
-export { default as CategoryForm } from "./components/category-form";
